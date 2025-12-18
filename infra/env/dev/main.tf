1f276aecb5b075367054a35ed2a2e7818e88f852
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-12-18 10:46:31 +0100

    refactor terraform code

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -1,8 +1,20 @@
+locals {
+  default_tags = {
+    project                 = var.project_name
+    team                    = var.team
+    environment             = var.env
+    "${var.company_name}:env" = var.env
+  }
+
+  
+  image_uri = "380426548948.dkr.ecr.eu-west-3.amazonaws.com/myproject-repo:lambda-20251218090602-fastapi-data-eng-dev"
+}
+ 
 module "lambda" {
   source = "../../module/lambda"
 
   function_name = "${var.project_name}-${var.env}-lambda"
-  image_uri     = var.image_uri
+  image_uri     = local.image_uri  # On utilise la valeur locale
   tags          = local.default_tags
 }
 
