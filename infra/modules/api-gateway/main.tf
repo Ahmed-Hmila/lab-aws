b17fb137a8328b9a8be768deed2729a5defe47f6
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-12-25 10:49:20 +0100

    add sqs to role

--- infra/modules/api-gateway/main.tf
+++ infra/modules/api-gateway/main.tf
@@ -93,17 +93,14 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   depends_on = [
-    aws_api_gateway_integration.sqs_integration 
+    aws_api_gateway_integration.sqs_integration
   ]
 
   triggers = {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.resource.id,
       aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.sqs_integration.id,
-      aws_api_gateway_resource.proxy.id,
-      aws_api_gateway_method.proxy_method.id,
-      aws_api_gateway_integration.lambda_proxy.id,
+      aws_api_gateway_integration.sqs_integration.id
     ]))
   }
 
