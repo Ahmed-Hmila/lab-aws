69525cbf1c98dd3ba7591937d9ee13cf2ea02b5c
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-12-26 11:42:45 +0100

    add crud

--- infra/modules/api-gateway/main.tf
+++ infra/modules/api-gateway/main.tf
@@ -1,3 +1,6 @@
+##########################
+# API Gateway REST API
+##########################
 resource "aws_api_gateway_rest_api" "api" {
   name = var.api_name
   tags = var.tags
@@ -7,57 +10,26 @@ resource "aws_api_gateway_rest_api" "api" {
   }
 }
 
-resource "aws_api_gateway_resource" "resource" {
+##########################
+# Submit (POST -> SQS)
+##########################
+resource "aws_api_gateway_resource" "submit_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "submit"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "submit_post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = aws_api_gateway_resource.submit_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-# Rôle IAM pour permettre à API Gateway d'envoyer des messages à SQS
-resource "aws_iam_role" "apigw_sqs_role" {
-  name = "${var.api_name}-sqs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "apigateway.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "apigw_sqs_policy" {
-  name = "apigw-sqs-policy"
-  role = aws_iam_role.apigw_sqs_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "sqs:SendMessage"
-        Effect   = "Allow"
-        Resource = var.sqs_queue_arn
-      }
-    ]
-  })
-}
-
-resource "aws_api_gateway_integration" "sqs_integration" {
+resource "aws_api_gateway_integration" "submit_sqs_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  resource_id             = aws_api_gateway_resource.submit_resource.id
+  http_method             = aws_api_gateway_method.submit_post.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${var.account_id}/${var.sqs_queue_name}"
@@ -67,40 +39,156 @@ resource "aws_api_gateway_integration" "sqs_integration" {
     "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
   }
 
-  # Transformation du corps de la requête en format SQS
   request_templates = {
     "application/json" = "Action=SendMessage&MessageBody=$util.urlEncode($input.body)"
   }
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
+resource "aws_api_gateway_method_response" "submit_response_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
+  resource_id = aws_api_gateway_resource.submit_resource.id
+  http_method = aws_api_gateway_method.submit_post.http_method
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "sqs_integration_response" {
+resource "aws_api_gateway_integration_response" "submit_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  resource_id = aws_api_gateway_resource.submit_resource.id
+  http_method = aws_api_gateway_method.submit_post.http_method
+  status_code = aws_api_gateway_method_response.submit_response_200.status_code
 
-  depends_on = [aws_api_gateway_integration.sqs_integration]
+  depends_on = [aws_api_gateway_integration.submit_sqs_integration]
 }
 
+##########################
+# Users (GET/POST -> Lambda)
+##########################
+resource "aws_api_gateway_resource" "users_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "users"
+}
+
+# GET /users
+resource "aws_api_gateway_method" "users_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.users_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "users_get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.users_resource.id
+  http_method             = aws_api_gateway_method.users_get.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+# POST /users
+resource "aws_api_gateway_method" "users_post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.users_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "users_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.users_resource.id
+  http_method             = aws_api_gateway_method.users_post.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+##########################
+# Users/{user_id} (GET/PUT/DELETE -> Lambda)
+##########################
+resource "aws_api_gateway_resource" "user_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.users_resource.id
+  path_part   = "{user_id}"
+}
+
+# GET /users/{user_id}
+resource "aws_api_gateway_method" "user_id_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.user_id_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "user_id_get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.user_id_resource.id
+  http_method             = aws_api_gateway_method.user_id_get.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+# PUT /users/{user_id}
+resource "aws_api_gateway_method" "user_id_put" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.user_id_resource.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "user_id_put_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.user_id_resource.id
+  http_method             = aws_api_gateway_method.user_id_put.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+# DELETE /users/{user_id}
+resource "aws_api_gateway_method" "user_id_delete" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.user_id_resource.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "user_id_delete_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.user_id_resource.id
+  http_method             = aws_api_gateway_method.user_id_delete.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+##########################
+# Déploiement
+##########################
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   depends_on = [
-    aws_api_gateway_integration.sqs_integration
+    aws_api_gateway_integration.submit_sqs_integration,
+    aws_api_gateway_integration.users_get_integration,
+    aws_api_gateway_integration.users_post_integration,
+    aws_api_gateway_integration.user_id_get_integration,
+    aws_api_gateway_integration.user_id_put_integration,
+    aws_api_gateway_integration.user_id_delete_integration
   ]
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.resource.id,
-      aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.sqs_integration.id
+      aws_api_gateway_resource.submit_resource.id,
+      aws_api_gateway_method.submit_post.id,
+      aws_api_gateway_resource.users_resource.id,
+      aws_api_gateway_method.users_get.id,
+      aws_api_gateway_method.users_post.id,
+      aws_api_gateway_resource.user_id_resource.id,
+      aws_api_gateway_method.user_id_get.id,
+      aws_api_gateway_method.user_id_put.id,
+      aws_api_gateway_method.user_id_delete.id
     ]))
   }
 
@@ -109,8 +197,6 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 }
 
- 
- 
 resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
