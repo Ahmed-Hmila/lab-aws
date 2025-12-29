daebadffa881cdce000d86ae397d8b30580ba5cd
Ahmed-Hmila <ahmed7hmila@gmail.com>
2025-12-29 11:39:10 +0100

    update sqs code

--- infra/modules/sqs/main.tf
+++ infra/modules/sqs/main.tf
@@ -1,10 +1,10 @@
 resource "aws_sqs_queue" "queue" {
   name                      = var.queue_name
-  delay_seconds             = 0
-  max_message_size          = 262144
-  message_retention_seconds = 345600
-  receive_wait_time_seconds = 0
-  visibility_timeout_seconds = 30
+  delay_seconds             = var.sqs_delay_seconds
+  max_message_size          = var.sqs_max_message_size
+  message_retention_seconds = var.sqs_message_retention_seconds
+  receive_wait_time_seconds = var.sqs_receive_wait_time_seconds
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
 
   tags = var.tags
 }
--- infra/modules/sqs/variables.tf
+++ infra/modules/sqs/variables.tf
@@ -17,3 +17,32 @@ variable "apigw_sqs_role_arn" {
   description = "ARN du rôle IAM pour permettre à API Gateway d'envoyer des messages à SQS"
   type        = string
 }
+variable "sqs_delay_seconds" {
+  description = "Délai avant que le message soit visible (en secondes)"
+  type        = number
+  default     = 0
+}
+
+variable "sqs_max_message_size" {
+  description = "Taille maximale du message SQS en octets"
+  type        = number
+  default     = 262144
+}
+
+variable "sqs_message_retention_seconds" {
+  description = "Durée de rétention des messages en secondes"
+  type        = number
+  default     = 345600
+}
+
+variable "sqs_receive_wait_time_seconds" {
+  description = "Temps d'attente pour le long polling"
+  type        = number
+  default     = 0
+}
+
+variable "sqs_visibility_timeout_seconds" {
+  description = "Durée pendant laquelle un message est invisible après lecture"
+  type        = number
+  default     = 30
+}
